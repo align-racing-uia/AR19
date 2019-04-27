2e8d56bcdc23342618763433d8eca982e8d53a12
Simon Nylund <a>
2019-04-27 12:09:40 +0200

    Added: Breaklight

---
+++ AR19_Breaklight/Breaklight_code/src/AllSettings.h
@@ -0,0 +1,19 @@
+//Breaklight Settings
+
+//Global CAN Settings
+const int breakPressureCANID = 0x19A;
+
+int breakPressure1 = 0 ;
+int breakPressure2 = 0 ;
+
+const int pressureThresholdOn = 12; //Threshold i hex av bremsen på 0 - 255 (Brems på)
+const int pressureThresholdOff = 8; //Threshold i hex av bremsen på 0 - 255 (Brems av)
+
+const int breaklightout = 3;
+
+//Debug Settings
+const int  rled = 5;
+const int  bled = 6;
+const int  gled = 9;
+
+
