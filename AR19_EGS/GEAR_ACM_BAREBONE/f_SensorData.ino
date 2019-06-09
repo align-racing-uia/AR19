e8ae35ed446b37eb79c7dcd16c862561acbd07c8
Henrik Brådland <henrbr16@student.uia.no>
2019-06-09 14:48:32 +0200

    Feilsoking av EGS-kode

--- AR19_EGS/GEAR_ACM_BAREBONE/f_SensorData.ino
+++ AR19_EGS/GEAR_ACM_BAREBONE/f_SensorData.ino
@@ -10,7 +10,7 @@ void CheckClutchPressure()
 {
   using namespace clutchpressure;
   sensorValue = analogRead(sensorPin);
-  InBar = map(sensorValue, 103, 921, 0, 250); // Remaps the bit value to conincide with BAR - info in datasheet
+  InBar = map(sensorValue, 103, 921, 0, 250); // Remaps the bit value to conincide with BAR - info in datasheet //Henrik: Mest sansynlig så bør innputsignalet mappes fra 0 til 255
 
   // Giver error code to the telemetry if clutch pressure is out of range.
   if (sensorValue < 103 || sensorValue > 921)
