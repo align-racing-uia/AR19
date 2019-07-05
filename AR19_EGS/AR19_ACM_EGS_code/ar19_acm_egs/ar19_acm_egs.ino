d707b7c565db9e799f9703cb4544f0915a394579
sandmag <sandmag@users.noreply.github.com>
2019-07-05 16:20:46 +0200

    Implemented functionality for calibrating EGS.
    Sends two bytes of sensor values when set to calibrate mode: gear voltage sensor and clutch pressure.

--- AR19_EGS/AR19_ACM_EGS_code/ar19_acm_egs/ar19_acm_egs.ino
+++ AR19_EGS/AR19_ACM_EGS_code/ar19_acm_egs/ar19_acm_egs.ino
@@ -2,7 +2,7 @@
 Align Racing UiA - Electronic Gear Shift
 
 Written by: Sander B. Johannessen
-June 2019
+Created: 05.07.2019
 
 Title: Electronic Gear Shift 
 Description: Code for controlling AR19 - Solan's gear shift
@@ -35,21 +35,21 @@ LedSettings led;
 
 struct can_frame msgIn;
 
-void gearUp();
-void gearDown();
-void gearNeutral();
+bool gearUp( uint8_t outputPin );
+bool gearDown( uint8_t outputPin );
+bool gearNeutral( uint8_t outputPin );
+uint16_t checkClutchPressure( uint8_t sensorPin );
+uint8_t checkGear( uint8_t sensorPin );
 
 void setup()
 {
-    //  Set pinMode and write LOW to PWM outputs to ensure a known state at startup
-    pinMode( etbcOut1Pin, OUTPUT );
-    pinMode( etbcOut2Pin, OUTPUT );
-    pinMode( etbcEnablePin, OUTPUT );
-    pinMode( etbcDisablePin, OUTPUT );
-    digitalWrite( etbcOut1Pin, LOW );
-    digitalWrite( etbcOut2Pin, LOW );
-    digitalWrite( etbcEnablePin, LOW );
-    digitalWrite( etbcDisablePin, LOW );
+    //  Set pinModes and writes LOW to output to ensure a stable startup state
+    pinMode( pinGearPositionSensor, INPUT_PULLUP );
+    pinMode( pinClutchPressureSensor, INPUT_PULLUP );
+    pinMode( pinGearUp, OUTPUT );
+    pinMode( pinGearDown, OUTPUT );
+    digitalWrite( pinGearUp, LOW );
+    digitalWrite( pinGearDown, LOW );
     
     //  Initialise CAN
     SPI.begin();
@@ -73,6 +73,13 @@ void loop()
     //  Loop while in calibration mode
     while ( calibrateMode ) {
         if ( millis() > calibrateTimestampLastMsg_ms + calibrateInterval_ms ) {
+            //  Read voltage from gear sensor (0-1023) and divides by 4 to send over CAN
+            gearSensorValue     = analogRead( pinGearPositionSensor ) /4;
+            //  Read voltage from clutch pressure sensor (0-1023) and divides by 4 to send over CAN
+            clutchPressureValue = analogRead( pinClutchPressureSensor ) /4;
+            //  Send raw values over CAN at defined calibration ID
+            can.send( canIdEgsCalibrate, gearSensorValue, clutchPressureValue );
+            //  Reset timer to send calibrate message at defined interval
             calibrateTimestampLastMsg_ms = millis();
         }
 
@@ -105,25 +112,87 @@ void loop()
             if ( msgIn.data[0] == globalTrue ) {
                 can.send( acmId, globalTrue );
             }
+
+        //  Check for gear shift request
+        } else if ( msgIn.can_id == canIdRequestGearShift ) {
+            if ( msgIn.data[0] == globalTrue && msgIn.data[1] == globalFalse ) {
+                
+                bool shiftSuccessful = gearUp( pinGearUp );
+
+                if ( shiftSuccessful ){
+                    led.ledsSwitch( led.green );
+                    shiftTimeout = true;
+                    shiftTimeoutLast_ms = millis();
+                } else {
+                    can.send( canIdEgsFaultFlag, faultCodeShiftUpFailure );
+                }
+
+            } else if ( msgIn.data[0] == globalFalse && msgIn.data[1] == globalTrue ) {
+                
+                bool shiftSuccessful = gearDown( pinGearDown );
+
+                if ( shiftSuccessful ){
+                    led.ledsSwitch( led.orange );
+                    shiftTimeout = true;
+                    shiftTimeoutLast_ms = millis();
+                } else {
+                    can.send( canIdEgsFaultFlag, faultCodeShiftDownFailure );
+                }
+
+            } else {
+                can.send( canIdEgsFaultFlag, faultCodeInvalidGearShiftRequest );
+            }
         }
-
-        //  Check for gear up request
-
-        //  Check for gear down request
 
         //  Check for neutral request
     }
 
+    //  Checks if timeout has passed 
+    if ( shiftTimeout ) {
+        if ( millis() > shiftTimeoutInterval_ms + shiftTimeoutLast_ms ) {
+            shiftTimeout = false;
+            led.ledsSwitch( led.off );
+        }
+    }
+
 }
 
-void gearUp(){
+bool gearUp( uint8_t outputPin ){
     //  implement
 }
 
-void gearDown(){
+bool gearDown( uint8_t outputPin ){
     //  implement
 }
 
-void gearNeutral(){
+bool gearNeutral( uint8_t outputPin ){
     //  implement
+}
+
+uint16_t checkClutchPressure( uint8_t sensorPin ){
+    uint16_t sensorValue = analogRead( sensorPin );
+    return sensorValue;
+}
+
+uint8_t checkGear( uint8_t sensorPin ){
+    uint16_t sensorValue = analogRead( sensorPin );
+    uint8_t currentGear;
+
+    if ( sensorValue > firstGearValue - gearMarginValue && sensorValue < firstGearValue + gearMarginValue ) {
+        currentGear = 1;
+    } else if ( sensorValue > neutralGearValue - gearMarginValue && sensorValue < neutralGearValue + gearMarginValue ) {
+        currentGear = 0;
+    } else if ( sensorValue > secondGearValue - gearMarginValue && sensorValue < secondGearValue + gearMarginValue ) {
+        currentGear = 2;
+    } else if ( sensorValue > thirdGearValue - gearMarginValue && sensorValue < thirdGearValue + gearMarginValue ) {
+        currentGear = 3;
+    } else if ( sensorValue > fourthGearValue - gearMarginValue && sensorValue < fourthGearValue + gearMarginValue ) {
+        currentGear = 4;
+    } else if ( sensorValue > fifthGearValue - gearMarginValue && sensorValue < fifthGearValue + gearMarginValue ) {
+        currentGear = 5;
+    } else if ( sensorValue > sixtGearValue - gearMarginValue && sensorValue < sixtGearValue + gearMarginValue ) {
+        currentGear = 6;
+    }
+
+    return currentGear;
 }
--- AR19_EGS/AR19_ACM_EGS_code/ar19_acm_egs/ar19_acm_egs_constants.h
+++ AR19_EGS/AR19_ACM_EGS_code/ar19_acm_egs/ar19_acm_egs_constants.h
@@ -14,83 +14,43 @@ const uint8_t globalFalse         = 0x0F;
 const uint16_t acmId  = 0x017;
 
 //  Definitions of CAN ID's
-const uint16_t canIdShutdown                    = 0x001;    //  ID for shutdown signal to shutdown ACM
-const uint16_t canIdCalibrateToggle             = 0x0F0;    //  ID for toggling calibration mode
-const uint16_t canIdAcmCheck                    = 0x019;    //  ID for ACM ping and status report
-const uint16_t canIdEgsFaultFlag                = 0x480;    //  ID for fault codes in EGS error state
+const uint16_t canIdShutdown            = 0x001;    //  ID for shutdown signal to shutdown ACM
+const uint16_t canIdCalibrateToggle     = 0x0F0;    //  ID for toggling calibration mode
+const uint16_t canIdAcmCheck            = 0x019;    //  ID for ACM ping and status report
+const uint16_t canIdEgsFaultFlag        = 0x480;    //  ID for fault codes in EGS error state
+const uint16_t canIdEgsCalibrate        = 0x481;    //  ID for sending calibration values 
+const uint16_t canIdRequestGearShift    = 0x240;    //  ID for recieving gear shift requests
 
-//  Definitions of EGS fault flag codes
-const uint8_t faultCodeThrottlePositionNotReached   = 10;
+//  Definitions of EGS fault flag codes (available range: 0-255)
+const uint8_t faultCodeInvalidGearShiftRequest  = 1;    //  Indicates error in gear shift request message recieved.
+const uint8_t faultCodeShiftDownFailure         = 2;    //  Indicates error in shifting up
+const uint8_t faultCodeShiftUpFailure           = 3;    //  Indicates error in shifting down
 
+//  Definition of external pins
+const uint8_t pinGearPositionSensor     = 18;
+const uint8_t pinGearUp                 = 3;
+const uint8_t pinGearDown               = 10;
+const uint8_t pinClutchPressureSensor   = 19;
 
-//  Definition of ACM pins controlling H-bridge to ETB
-const uint8_t etbcOut1Pin     = 10;
-const uint8_t etbcOut2Pin     = 3;
-const uint8_t etbcEnablePin   = 11;
-const uint8_t etbcDisablePin  = 12;
-const uint8_t etbcFeedbackPin = 19;
+//  Definition of gear related variables
+const uint16_t gearMarginValue      = 10;   //  Adjustable margin for calculating current gear
+const uint16_t neutralGearValue     = 0;
+const uint16_t firstGearValue       = 0;
+const uint16_t secondGearValue      = 0;
+const uint16_t thirdGearValue       = 0;
+const uint16_t fourthGearValue      = 0;
+const uint16_t fifthGearValue       = 0;
+const uint16_t sixtGearValue        = 0;
+uint16_t currentGear, gearSensorValue;
+bool shiftTimeout                   = false;    //  Shift timeout to prevent excessive shifting
 
-//  Definition of TPS related variables
-const uint8_t tps1Pin = 16;
-const uint8_t tps2Pin = 17;
-uint16_t tps1Value;
-uint16_t tps2Value;
-uint8_t tps1Out;
-uint8_t tps2Out;
-int16_t tpsDifference;
-uint8_t tpsDifference_percent;
-
-//  Variables used for implausibility checks
-bool tps2ImplausibilityOutofRangeMax        = false;
-bool tps2ImplausibilityOutofRangeMin        = false;
-bool tps1ImplausibilityOutofRangeMax        = false;
-bool tps1ImplausibilityOutofRangeMin        = false;
-uint8_t tpsImplausible                      = 0;
-unsigned long tpsLastDiffImplausibility_ms  = 0;
-
-//  Variables used in comparing target with actual position
-const unsigned long reachThrottleTargetLimit_ms = 500;
-unsigned long reachThrottleTargetTimer_ms       = 1000;
-
-//  Max and min values for TPS1 and 2
-const uint16_t tps1Limp         = 296;  //  296 med BSPD
-const uint16_t tps1Min          = 192;  //  192 med BSPD
-const uint16_t tps1Max          = 864;  //  864 med BSPD
-uint16_t tps1Idle               = 255;  //  230 med BSPD, Økte til 255 for å øke idle til 9 % (lest av i Tunerstudio) 
-uint16_t tps1IdleOld            = tps1Idle;  //  old idle value when enabling/disabling auto idle
-const uint16_t tps2Min          = 838;  //  838 med BSPD
-const uint16_t tps2Max          = 156;  //  156 med BSPD
-const uint16_t tps1PhysicalMax  = 972;  //  Mechanical stop with BSPD
-const uint16_t tps2PhysicalMax  = 48;   //  Mechanical stop with BSPD
-
-//  Variables used for mapping throttle target
-uint8_t apps1Remap;
-uint8_t apps1Value;
-uint8_t apps2Value;
-uint8_t appsDifference_percent;
-
-//  Variables used in auto idle from RPM. Default=1700 RPM
-uint16_t rpmValue;
-uint16_t rpmIdle    = 1700;
-
-// Variable to set setpoint from LC 
-uint8_t lcTargetValue;
-
-//  Variables used in check for reaching limp mode 
-bool limpStarted                            = false;
-bool throttleInLimp                         = false;
-const unsigned long reachLimpInterval_ms    = 1000;
-unsigned long limpModeTimer_ms              = 1000;
+//  Definition of clutch related variables
+uint16_t clutchPressureValue        = 0;
 
 //  Time variables used for CAN msg intervals 
-const unsigned long tpsInterval_ms                  = 100;
-unsigned long tpsTimestampLastMsg_ms                = 0;
-const unsigned long calibrateInterval_ms            = 100;
-unsigned long calibrateTimestampLastMsg_ms          = 0;
-const unsigned long blipDuration_ms                 = 100;
-const unsigned long blipIntervalLimit_ms            = 1000;
-unsigned long blipTimestampLastMsg_ms               = 0;
-const unsigned long tpsImplausibilityInterval_ms    = 100;
-unsigned long tpsImplausibilityLastMsg_ms           = 0;
-const unsigned long limpMsgInterval_ms              = 100;
-unsigned long lastLimpMsg_ms                        = 0;
+const unsigned long calibrateInterval_ms    = 100;  //  Interval [ms] for calibration messages
+unsigned long calibrateTimestampLastMsg_ms  = 0;
+
+//  Time variables for other functions
+const unsigned long shiftTimeoutInterval_ms = 100;  //  Interval [ms] until next gear shift is allowed
+unsigned long shiftTimeoutLast_ms           = 0;
